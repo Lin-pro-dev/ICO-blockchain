91ecc14aac42db2dff46708b1393cd828bed0fd7
Lin-pro-dev <lincaspianpro@gmail.com>
2024-06-14 06:16:02 -0400

    add react fronend

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import Web3 from 'web3'
+import './App.css';
+
+const App = () => {
+
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
